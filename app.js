ffc85734154f86c6f5ecb71149681154baaeb8ab
mindx-jsa <148680155+mindx-jsa@users.noreply.github.com>
2023-11-11 09:17:08 +0700

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+// let x=3
+// let y=8
+// let z=4
+// let max=x
+// if (z>y){
+// if (z>x){
+//     console.log("z");    
+// }
+// else{
+//     console.log("y")
+// }
+// }
+// else{
+//     if(x>y){
+//         console.log("x")
+//     }
+//     else{
+//         console.log("y")
+//     }
+// }
+
+
+// let x=1
+// let y=3 
+// let z=5
+// let min=x
+
+// if(min>y){
+//     min=y
+// }
+// if(min>z){
+//     min=z
+// }
+// console.log(min);
+
+
+// let greet = function(name) {
+//     console.log("hello"+name+"!");
+// }
+// greet(" Khoi Nguyen");
+// function min(a, b){
+//     return a < b ? a : b;
+// }
+// console.log(min(6, 7));
+// greet("Thao");
+
+// function increase(number){
+//     let x=5;
+//     number +=x}
+
+
